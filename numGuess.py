17bff52a8b31be733b568ea3361cf5d032882a66
Anthony Bosek <anthony.bosek@gmail.com>
2023-05-05 05:53:17 -0700

    update formatter

--- numGuess.py
+++ numGuess.py
@@ -6,10 +6,10 @@ if top_of_range.isdigit():
     top_of_range = int(top_of_range)
 
     if top_of_range <= 0:
-        print('Please type a number larger than 0 next time.')
+        print("Please type a number larger than 0 next time.")
         quit()
 else:
-    print('Please type a number next time.')
+    print("Please type a number next time.")
     quit()
 
 random_number = random.randint(0, top_of_range)
@@ -21,7 +21,7 @@ while True:
     if user_guess.isdigit():
         user_guess = int(user_guess)
     else:
-        print('Please type a number next time.')
+        print("Please type a number next time.")
         continue
 
     if user_guess == random_number:
--- realFan.py
+++ realFan.py
@@ -12,7 +12,7 @@
 
 # print('Your favorite football team is ' + football + '.')
 
-# 
+#
 print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
@@ -25,28 +25,28 @@ score = 0
 
 answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
-    print('Correct!')
+    print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does GPU stand for? ")
 if answer.lower() == "graphics processing unit":
-    print('Correct!')
+    print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does RAM stand for? ")
 if answer.lower() == "random access memory":
-    print('Correct!')
+    print("Correct!")
     score += 1
 else:
     print("Incorrect!")
 
 answer = input("What does PSU stand for? ")
 if answer.lower() == "power supply":
-    print('Correct!')
+    print("Correct!")
     score += 1
 else:
     print("Incorrect!")
